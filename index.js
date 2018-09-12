12e90ddaeef6c89c7ea490a57900b6e5472330b2
Oliver Sierra <oliver.sierra@ignition.sapient.com>
2018-09-11 21:00:34 -0700

    Adding joi for request body validation and Adding delete endpoint

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
+
 const port = process.env.PORT || 3000;
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()  
+  };
+
+  return Joi.validate(course, schema);
+}
 
 app.use(express.json());
 
@@ -14,17 +24,27 @@ app.get('/', (req, res) => {
   res.send('Hello World API');
 });
 
+
+// Retrieve all curses
 app.get('/api/courses', (req, res) => {
   res.send(courses);
 });
 
+// Retrieve course by id
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the given id was not found');
+
+  if (!course) return res.status(404).send('The course with the given id was not found');
+
   res.send(course);
 });
 
+// Add a course
 app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -32,6 +52,29 @@ app.post('/api/courses', (req, res) => {
 
   courses.push(course);
 
+  res.send(course);
+});
+
+// Update course by id
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  const { error } = validateCourse(req.body);
+
+  if (!course) return res.status(404).send('The course with the given id was not found');
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+// Delete course
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given id was not found');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
   res.send(course);
 });
 
